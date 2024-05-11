feeb4ea35edb6de9f3b90c697b981b603a07f3bc
bherzherk <bherzherk@gmail.com>
2024-05-11 11:31:02 -0400

    Server threading function, and client send receive message to the server for multi chat.

--- client.py
+++ client.py
@@ -4,14 +4,28 @@ import threading
 from customtkinter import *
 from tkinter.scrolledtext import ScrolledText
 
-def send_message(event, client_socket, username, text_chat, entry_chat):
+def send_message(client_socket, username, text_chat, entry_chat):
     message = entry_chat.get()
     client_socket.sendall(f"{username} > {message}".encode())
 
     entry_chat.delete(0, END)
-    text_chat.configure(state='normal')
-    text_chat.insert("end", f"{username} > {message}\n")
-    text_chat.configure(state='disabled')
+    #text_chat.configure(state='normal')
+    #text_chat.insert("end", f"{username} > {message}\n")
+    #text_chat.configure(state='disabled')
+
+def receive_mesage(client_socket, text_chat):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+            if not message:
+                break
+
+            text_chat.configure(state='normal')
+            text_chat.insert("end", message)
+            text_chat.configure(state='disabled')
+        except:
+            break
+
 
 def run_client():
     host = 'localhost'
@@ -33,8 +47,12 @@ def run_client():
     text_chat.pack(padx=5, pady=5)
 
     entry_chat = CTkEntry(window)
-    entry_chat.bind("<Return>", lambda event: send_message(event, client_socket, username, text_chat, entry_chat))
+    entry_chat.bind("<Return>", lambda _: send_message(client_socket, username, text_chat, entry_chat))
     entry_chat.pack(padx=5, pady=5, fill=BOTH)
+
+    thread = threading.Thread(target=receive_mesage, args=(client_socket, text_chat))
+    thread.daemon = True
+    thread.start()
 
     window.mainloop()
     client_socket.close()
--- server.py
+++ server.py
@@ -9,7 +9,23 @@ def client_thread(client_socket, clients, usernames):
     username  = client_socket.recv(1024).decode()
     usernames[client_socket] = username
 
-    print(f"The username is: {username}")
+    #print(f"The username is: {username}")
+    
+    for client in clients:
+        if client is not client_socket:
+            client.sendall(f"\n[+] The user {username} has joined to the chat.\n".encode())
+
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+
+            if not message:
+                break
+
+            for client in clients:
+                client.sendall(f"{message}\n".encode())
+        except:
+            break
 
 def run_server():
     host = 'localhost'
