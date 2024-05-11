b5aacac49515970121e3c6eeec0123616d08ca4b
ChristianPrjkts <christian.mendoza2pw@gmail.com>
2024-05-10 22:18:38 -0400

    client adding CTk, floating window

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import socket
+import threading
+from customtkinter import *
+from tkinter.scrolledtext import ScrolledText
 
 def run_client():
     host = 'localhost'
-    port = 123435
+    port = 12345
     destination_addr = (host, port)
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +14,21 @@ def run_client():
 
     username = input("\n[+] Enter username: ")
     client_socket.sendall(username.encode())
+
+    # Window
+    window = CTk()
+    window.title("Chat")
+    set_appearance_mode("dark")
+
+    text_chat = ScrolledText(window, state='disabled')
+    text_chat.pack(padx=5, pady=5)
+
+    entry_chat = CTkEntry(window)
+#    entry_chat.bind("Return", lambda)
+    entry_chat.pack(padx=5, pady=5, fill=BOTH)
+
+    window.mainloop()
+    client_socket.close()
 
 if __name__ == "__main__":
     run_client()
